f63c3b5736c67b7a7a95740b0a3ba45a4acf087c
leekthang <thangkaba@gmail.com>
2022-11-17 18:41:03 +0700

    Thang Commit Test

--- app/src/main/java/com/example/da1_poly_n6/FragmentManager/AccountFrgm.java
+++ app/src/main/java/com/example/da1_poly_n6/FragmentManager/AccountFrgm.java
@@ -15,7 +15,7 @@ public class AccountFrgm extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_frgm, container, false);
+        View view = inflater.inflate(R.layout.fragment_account_frgm, container, false);
+        return view;
     }
 }
